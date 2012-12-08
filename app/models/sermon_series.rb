fbe5d0262d6acbdf6f1c51aa7da4d54c9ff02b47
Derek Barber <db@derekbarber.ca>
2012-12-08 13:50:53 -0800

    Added the image_url attr_accessible calls to the model

--- app/models/sermon_series.rb
+++ app/models/sermon_series.rb
@@ -1,7 +1,7 @@
 class SermonSeries < ActiveRecord::Base
   has_many :sermons
 
-  attr_accessible :banner_image, :button_image, :current_series, :description, :end_date, :start_date, :title
+  attr_accessible :banner_image, :button_image, :current_series, :description, :end_date, :start_date, :title, :banner_image_url, :button_image_url
   mount_uploader :banner_image, SermonImageUploader
   mount_uploader :button_image, SermonImageUploader
 end
