d21f078170b1d31ad3deef80018e7a31b32e0566
Derek Barber <db@derekbarber.ca>
2014-01-30 09:26:11 -0800

    Add full_title method

--- app/models/sermon.rb
+++ app/models/sermon.rb
@@ -3,4 +3,8 @@ class Sermon < ActiveRecord::Base
   
   #attr_accessible :audio_file, :date, :description, :scripture_reference, :sermon_series_id, :speaker, :title, :video_embed, :audio_file_duration
   mount_uploader :audio_file, SermonAudioUploader
+  
+  def full_title
+    scripture_reference.blank? ? "#{title}" : "#{title} (#{scripture_reference})"
+  end
 end
