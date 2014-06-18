2ff363c97ca242d5a2e0283a6865699471a3087e
Derek Barber <db@derekbarber.ca>
2014-06-17 18:38:32 -0700

    Try some different environment settings for staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -8,18 +8,18 @@ Crossway::Application.configure do
 
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
+  #config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  #config.serve_static_assets = false
 
   config.assets.js_compressor = :uglifier
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  #config.assets.compile = true
 
   # Generate digests for assets URLs
-  config.assets.digest = true
+  #config.assets.digest = true
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
