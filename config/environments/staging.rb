9ce0e6a092d6e284cdc774a373e425f63c116ad2
Derek Barber <db@derekbarber.ca>
2014-06-18 10:51:53 -0700

    Turn off asset features for staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -11,15 +11,15 @@ Crossway::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   config.assets.js_compressor = :uglifier
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs
-  config.assets.digest = true
+  config.assets.digest = false
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
