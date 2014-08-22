ff1fbef4a56023f29663aa77b31ff9b39185d125
Derek Barber <db@derekbarber.ca>
2014-08-21 22:31:24 -0700

    Add separate devise layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  layout :layout_by_resource
+
+  def layout_by_resource
+    if devise_controller? #&& resource_name == :user && action_name == 'sign_in'
+      "devise"
+    else
+      "application"
+    end
+  end
 end
